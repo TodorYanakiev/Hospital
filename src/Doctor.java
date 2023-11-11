5cc7cafa28c71d9800daf8c0853a80c48b799776
TodorYanakiev <todoryanakiev395@gmail.com>
2023-11-11 20:07:54 +0200

    getter setters added.

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,4 +3,36 @@ public class Doctor {
     private String firstName;
     private String lastName;
     private String speciality;
+
+    public int getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(int doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
 }
