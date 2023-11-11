11dff07b03087e8021c83833e735a1637bb08b52
TodorYanakiev <todoryanakiev395@gmail.com>
2023-11-11 20:23:15 +0200

    doctor class added

---
+++ src/Doctor.java
@@ -0,0 +1,38 @@
+public class Doctor {
+    private int doctorID;
+    private String firstName;
+    private String lastName;
+    private String specialty;
+
+    public int getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(int doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+}
