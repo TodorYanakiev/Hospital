f27ff68f29af3554c21210c28f0ca8a4831653c9
TodorYanakiev <todoryanakiev395@gmail.com>
2023-11-27 01:02:40 +0200

    m

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-    public static String getUserRole() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your role (patient or doctor):");
-        String role = sc.next();
-        role.toLowerCase();
-        while (true) {
-            if (role.equals("doctor") || role.equals("patient")) {
-                break;
-            }
-            System.out.println("Your role must be \"patient\" or \"doctor\": ");
-            role = sc.next();
-        }
-        return role;
-    }
-
     public static void main(String[] args) {
-        if (getUserRole().equals("doctor")) {
 
-        } else {
-
-        }
     }
 }
