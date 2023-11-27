7f0f61fa79d3b73a7323c7269b5cbe49b809c792
TodorYanakiev <todoryanakiev395@gmail.com>
2023-11-27 21:58:07 +0200

    Constructor appointment.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 public class Main {
     public static void main(String[] args) {
-        LocalTime d = LocalTime.parse("11:03");
-        System.out.println(d);
     }
 }
