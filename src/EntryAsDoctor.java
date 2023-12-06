c42212787151815b193aa6cff3331699aa5b5989
TodorYanakiev <todoryanakiev395@gmail.com>
2023-12-07 00:34:48 +0200

    When you enter as doctor it gets your id and first name.

---
+++ src/EntryAsDoctor.java
@@ -0,0 +1,46 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class EntryAsDoctor {
+    public static void Entry() {
+        System.out.println("Please enter your doctor ID: ");
+        Scanner sc = new Scanner(System.in);
+        int doctorID = getDoctorID(sc);
+        System.out.println("Please enter your first name(use Capital letter): ");
+        String doctorFirstName = sc.next();
+        if (isDoctorExisting(doctorID, doctorFirstName)) {
+            System.out.println("Your doctor is real!");
+        } else {
+            while (!isDoctorExisting(doctorID,doctorFirstName)){
+                System.out.println("There is no such a doctor! Enter your doctor ID again:");
+                doctorID = getDoctorID(sc);
+                System.out.println("Please enter your first name again(use Capital letter): ");
+                doctorFirstName = sc.next();
+            }
+            System.out.println("Your doctor is real!");
+        }
+    }
+    private static int getDoctorID(Scanner sc){
+        String textDoctorID = sc.next();
+        int doctorID;
+        while (true) {
+            if (textDoctorID.matches("^\\d+$")){
+                doctorID = Integer.parseInt(textDoctorID);
+                break;
+            }else {
+                System.out.println("The doctor ID must be a whole number! Enter the ID again: ");
+                textDoctorID = sc.next();
+            }
+        }
+        return doctorID;
+    }
+
+    private static boolean isDoctorExisting(int doctorID, String doctorFirstName) {
+        CSVReader csvReader = new CSVReader();
+        List<Doctor> doctorList = csvReader.readDoctorList("doctors.csv");
+        for (Doctor doctor : doctorList) {
+            if (doctor.getDoctorID() == doctorID && doctor.getFirstName().equals(doctorFirstName)) return true;
+        }
+        return false;
+    }
+}
