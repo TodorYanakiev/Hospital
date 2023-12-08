499cf9a5a2fd4ca17108ed85202fa0159e897ff4
TodorYanakiev <todoryanakiev395@gmail.com>
2023-12-09 01:16:41 +0200

    test for appointmentAdder

--- test/CSVWriterTest.java
+++ test/CSVWriterTest.java
@@ -22,21 +22,6 @@ public class CSVWriterTest {
     }
 
     @Test
-    public void addAppointmentTestMethodCalledTwice(){
-        //GIVEN
-        Appointment app1 = new Appointment(2,3,"procedure",(LocalDate.parse("2024-01-17")),
-                930,6);
-        CSVReader reader = new CSVReader();
-        List<Appointment> appointmentList = reader.readAppointmentList("appointmentsTest2.csv");
-        //WHEN
-        CSVWriter.addAppointment(app1, new File("appointmentsTest2.csv"));
-        CSVWriter.addAppointment(app1, new File("appointmentsTest2.csv"));
-        //THEN
-        boolean isAppointmentInTheList = appointmentList.stream().anyMatch(appointment -> appointment.equals(app1));
-        Assertions.assertTrue(isAppointmentInTheList);
-    }
-
-    @Test
     public void addAppointmentTestGivenNull(){
         //GIVEN
         Appointment app1 = null;
