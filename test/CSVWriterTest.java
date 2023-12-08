7d646d2f001a762df57e7c2aa690f9ea39a1dd17
TodorYanakiev <todoryanakiev395@gmail.com>
2023-12-08 22:48:50 +0200

    addAppointmentTest added.

---
+++ test/CSVWriterTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDate;
+import java.util.List;
+
+public class CSVWriterTest {
+    @Test
+    public void addAppointmentTest(){
+        //GIVEN
+        Appointment app1 = new Appointment(2,3,"procedure",(LocalDate.parse("2024-01-17")),930,6);
+        CSVReader reader = new CSVReader();
+        List<Appointment> appointmentList = reader.readAppointmentList("appointmentsTest2.csv");
+        //WHEN
+        CSVWriter.addAppointment(app1, new File("appointmentsTest2.csv"));
+        //THEN
+        boolean isAppointmentInTheList = appointmentList.stream().anyMatch(appointment -> appointment.equals(app1));
+        Assertions.assertTrue(isAppointmentInTheList);
+    }
+}
