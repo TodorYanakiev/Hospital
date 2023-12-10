aa9ef11c58e341df89e9c767a8605a513e845b2e
TodorYanakiev <todoryanakiev395@gmail.com>
2023-12-10 11:52:47 +0200

    option selector construction

--- src/DoctorOptionSelector.java
+++ src/DoctorOptionSelector.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class DoctorOptionSelector {
-    public void selectOption(int doctorID){
+    public void selectOption(int doctorID) {
+        System.out.println("Chose one from the options(write only the number):");
+        System.out.println("1. Show all appointments for a doctor.");
+        System.out.println("2. Sort  all appointments for a doctor.");
+        System.out.println("3. Group patients.");
+        Scanner sc = new Scanner(System.in);
+        String option = sc.next();
+        while (true) {
+            switch (option) {
+                case "1":
+                    showAllAppointments();
+                    break;
+                case "2":
+                    sortAllAppointments();
+                    break;
+                case "3":
+                    groupPatients();
+                    break;
+                default:
+                    System.out.println("Invalid option! Please enter again:");
+                    option = sc.next();
+            }
+        }
+    }
+
+    public void showAllAppointments() {
+
+    }
+
+    public void sortAllAppointments() {
+
+    }
+
+    public void groupPatients() {
 
     }
 }
