1021445a31e5ca44666231a0487371b0ad032922
TodorYanakiev <todoryanakiev395@gmail.com>
2023-12-10 11:17:08 +0200

    test for adding Doctor

--- test/CSVWriterTest.java
+++ test/CSVWriterTest.java
@@ -1,15 +1,41 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.time.LocalDate;
 import java.util.List;
 
 public class CSVWriterTest {
     @Test
-    public void addAppointmentTestCorrectParameters(){
+    public void addDoctorTestCorrectParameters(){
         //GIVEN
-        Appointment app1 = new Appointment(2,3,"procedure",(LocalDate.parse("2024-01-17")),
+        Doctor doctor1 = new Doctor(IDMaker.makeDoctorID("doctorsTest1.csv"),"Nikolay","Simeonov","cardiology");
+        CSVReader reader = new CSVReader();
+        //WHEN
+        CSVWriter.addDoctor(doctor1,new File("doctorsTest1.csv"));
+        List<Doctor> doctorList = reader.readDoctorList("doctorsTest1.csv");
+        //THEN
+        boolean isDoctorInTheList = doctorList.stream().anyMatch(doctor -> doctor.equals(doctor1));
+        Assertions.assertTrue(isDoctorInTheList);
+    }
+
+    @Test
+    public void addDoctorTestGivenNull(){
+        //GIVEN
+        Doctor doctor1 = null;
+        CSVReader reader = new CSVReader();
+        //WHEN
+        CSVWriter.addDoctor(doctor1, new File("doctorsTest2.csv"));
+        List<Doctor> doctorList = reader.readDoctorList("doctorsTest2.csv");
+        //THEN
+        boolean isDoctorInTheList = doctorList.stream().anyMatch(doctor -> doctor.equals(doctor1));
+        Assertions.assertFalse(isDoctorInTheList);
+    }
+
+    @Test
+    public void addAppointmentTestGivenCorrectParameters(){
+        //GIVEN
+        Appointment app1 = new Appointment(IDMaker.makeAppointmentID("appointmentsTest1.csv"),3,
+                "procedure",(LocalDate.parse("2024-01-17")),
                 930,6);
         CSVReader reader = new CSVReader();
         //WHEN
@@ -25,9 +51,9 @@ public class CSVWriterTest {
         //GIVEN
         Appointment app1 = null;
         CSVReader reader = new CSVReader();
-        List<Appointment> appointmentList = reader.readAppointmentList("appointmentsTest2.csv");
         //WHEN
         CSVWriter.addAppointment(app1, new File("appointmentsTest2.csv"));
+        List<Appointment> appointmentList = reader.readAppointmentList("appointmentsTest2.csv");
         //THEN
         boolean isAppointmentInTheList = appointmentList.stream().anyMatch(appointment -> appointment.equals(app1));
         Assertions.assertFalse(isAppointmentInTheList);
