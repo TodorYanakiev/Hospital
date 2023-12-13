91e9eba20a5fe5617a9ebc183516efcbd8e9ea97
mecho420 <ahmedhasanovahmedcho@gmail.com>
2023-12-13 22:53:49 +0200

    Fixed Option 2

--- src/VisualiseAllAppointmentsFoPatient.java
+++ src/VisualiseAllAppointmentsFoPatient.java
@@ -5,42 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VisualiseAllAppointmentsFoPatient {
-    private static final String APPOINTMENTS_FILE = "appointments.csv";
-    private static final String DOCTORS_FILE = "doctors.csv";
+    private String appointmentFileName;
+    private String doctorFileName;
+    private String patientFileName;
+
+    public VisualiseAllAppointmentsFoPatient(String appointmentFileName, String doctorFileName, String patientFileName) {
+        this.appointmentFileName = appointmentFileName;
+        this.doctorFileName = doctorFileName;
+        this.patientFileName = patientFileName;
+    }
 
     public void visualizePatientAppointments(int patientId) {
-        try {
-            BufferedReader appointmentsReader = new BufferedReader(new FileReader(APPOINTMENTS_FILE));
-            BufferedReader doctorsReader = new BufferedReader(new FileReader(DOCTORS_FILE));
-
-            List<String[]> appointments = new ArrayList<>();
-            List<String[]> doctors = new ArrayList<>();
-
-            String line;
-            while ((line = appointmentsReader.readLine()) != null) {
-                String[] appointmentData = line.split(",");
-                appointments.add(appointmentData);
-            }
-
-            while ((line = doctorsReader.readLine()) != null) {
-                String[] doctorData = line.split(",");
-                doctors.add(doctorData);
-            }
-
-            boolean foundAppointments = false;
-            for (String[] appointment : appointments) {
-                if (appointment[1].equals(String.valueOf(patientId))) {
-                    foundAppointments = true;
-                }
-            }
-
-            if (!foundAppointments) {
-                System.out.println("This patient has no appointments.");
-            }
-            appointmentsReader.close();
-            doctorsReader.close();
-        } catch (IOException e) {
-            System.err.println("Error: The file for the appointments is not found.");
-        }
+        CSVReader csvReader = new CSVReader();
+        List<Appointment> allAppointments = csvReader.readAppointmentList(appointmentFileName);
+        List<Appointment> filteredAppointments = allAppointments.stream().filter(appointment -> appointment.getPatientID() == patientId).toList();
+        filteredAppointments.forEach(System.out::println);
     }
 }
